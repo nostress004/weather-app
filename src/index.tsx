4c1954c603db6850266daf96b35c9a393deadd25
KIGYOSI <csaba.kigyosi@lhsystems.com>
2019-08-14 14:54:29 +0200

    remove unneccessary dependency

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import 'font-awesome/css/font-awesome.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
