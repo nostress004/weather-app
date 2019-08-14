100d4319ef911e3c65ef08b6ac3cf2b5c6e960da
KIGYOSI <csaba.kigyosi@lhsystems.com>
2019-08-14 14:31:00 +0200

    add textfield as fallback

--- src/WeatherInfoDisplay.tsx
+++ src/WeatherInfoDisplay.tsx
@@ -2,28 +2,25 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import WeatherType from './types/Weather';
 import Position from './types/Position';
+import PositionError from './types/PositionError';
 import './weather.css';
 
-const WEATHER_API_KEY = 'd8307bc01685cdccd72639be4759b286';
+const WEATHER_API_KEY = '67016261fe6f47307e52b966d81605ef';
 
 export default function WeatherInfoDisplay() {
     const [weatherInfo, setWeatherInfo] = useState<WeatherType>();
+    const [loaded, setLoaded] = useState<boolean>(false);
     const [location, setLocation] = useState<Position>();
+    const [currentCity, setCurrentCity] = useState<string>('');
     const defaultCity = 'Budapest';
 
     useEffect(() => {
-        if(!weatherInfo) {
-            getLocation();
-        }
-    });
+        getLocation();
+    }, []);
 
     function getLocation() {
-        const options = {};
-
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(successCallback);
-        } else {
-            debugger;
+            navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
         }
     }
 
@@ -34,20 +31,52 @@ export default function WeatherInfoDisplay() {
             {params: {lat: coords.latitude, lon: coords.longitude, appid:WEATHER_API_KEY, units: 'metric'}})
             .then(function(response) {
                 setWeatherInfo(response.data);
+                setLoaded(true);
             });
     }
 
-    function errorCallback(position: Position) {
+    function errorCallback(position: PositionError) {
+        setLoaded(true);
+    }
+
+    function getWeatherByCity(city: string) {
         axios.get('http://api.openweathermap.org/data/2.5/weather',
-            {params: {city: 'Budapest', appid:WEATHER_API_KEY, units: 'metric'}})
+            {params: {q: city, appid:WEATHER_API_KEY, units: 'metric'}})
             .then(function(response) {
-                setWeatherInfo(response.data);
-            });
+                 setWeatherInfo(response.data);
+                 setLoaded(true);
+             });
+    }
+
+    function submitCityForm(event: React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+
+        if (currentCity) {
+            getWeatherByCity(currentCity);
+        }
+    }
+
+    function handleChange(event: React.FormEvent<HTMLInputElement>) {
+        const target = event.target as HTMLTextAreaElement;
+        setCurrentCity(target.value);
     }
 
     return <div>
-            <p className="display-1 degree">{weatherInfo && weatherInfo.main && weatherInfo.main.temp} °C</p>
-            <p>{weatherInfo && weatherInfo.name}</p>
-            <i className="fa fa-sun-o loading-spinner"/>
+        {weatherInfo &&
+            <div className=''>
+                <p className="display-1 degree">{weatherInfo && weatherInfo.main && weatherInfo.main.temp} °C</p>
+                <p>{weatherInfo && weatherInfo.name}</p>
+                <p>{weatherInfo && weatherInfo.weather && weatherInfo.weather[0].main}</p>
+                <img src={`http://openweathermap.org/img/wn/${ weatherInfo && weatherInfo.weather && weatherInfo.weather[0].icon}@2x.png`} />
+            </div>}
+            {!weatherInfo && loaded &&
+                <form onSubmit={submitCityForm}>
+                    <small>We could not get your current location, please enter your current city</small>
+                    <div className="form-group">
+                        <input className="input-field" placeholder="Budapest" onChange={handleChange}/>
+                        <button type="submit" className="btn btn-outline-success submitbutton">Submit</button>
+                    </div>
+                </form>
+            }
         </div>;
 }
---
+++ src/types/PositionError.ts
@@ -0,0 +1,4 @@
+export default interface PositionError {
+    code: number;
+    message: string;
+}
